4b9eeaf00040b490ba4c3ac243ac8dac594e6412
Devika Menon <appu121010devi@gmail.com>
2020-07-02 19:52:41 +0530

    bgchanged

--- sketch.js
+++ sketch.js
@@ -7,18 +7,19 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var score = 0;
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getbackgroundimg();
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+   
+   
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -45,18 +46,25 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg){
     background(backgroundImg);
     Engine.update(engine);
     //strokeWeight(4);
+    noStroke();
+    fill("yellow");
+    textSize(35); 
+    text("Score: "+ score, width-300,50);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -66,7 +74,8 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();   
+    } 
 }
 
 function mouseDragged(){
@@ -85,4 +94,23 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getbackgroundimg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJ = await response.json();
+    console.log(responseJ);
+    console.log(responseJ.datetime);
+    var datetime = responseJ.datetime ;
+   var hour = datetime.slice(11,13);
+   console.log(hour);
+
+   if(hour >= 06 && hour <= 18){
+       bg = "sprites/bg.png";
+
+
+   }else{
+      bg = "sprites/bg2.jpg";
+   }
+   backgroundImg = loadImage(bg);
 }
